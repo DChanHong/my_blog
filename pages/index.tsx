41765759896c81c99d3a75aff0f79d69f97e4f07
Chanhong Sung <99067250+DChanHong@users.noreply.github.com>
2023-11-17 00:14:18 +0900

    feat : blog css complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,19 @@ import Layout from "@/components/layout/Layout";
 export default function Home() {
   return (
     <Layout>
-      <section className=" w-9/12 border- m-auto">
+      <section className=" w-8/12 m-auto">
         <h3 className="font-bold text-[2.5em]">Career</h3>
-        <div className="my-2 text-[2em]">
-          <p>나를 소개합니다.</p>
+        <div className="my-2 border-b-2 py-4">
+          <p className="text-[2em]">나를 소개합니다.</p>
         </div>
         <h3 className="font-bold text-[2.5em]">My Blog List</h3>
-
+        <div className="my-2 border-b-2 py-4 ">
+          <p className="text-[2em]">나를 소개합니다.</p>
+        </div>
         <h3 className="font-bold text-[2.5em]">기술 스택</h3>
+        <div className="my-2 border-b-2 py-4 ">
+          <p className="text-[2em]">나를 소개합니다.</p>
+        </div>
       </section>
     </Layout>
   );
